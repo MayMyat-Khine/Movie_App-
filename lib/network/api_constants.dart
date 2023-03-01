9824ebf4c3b9ebc704f7da7e3ae5835da42004d3
khinemyat <khinemyat312@gmail.com>
2023-03-01 20:38:47 +0630

    dio and http

---
+++ lib/network/api_constants.dart
@@ -0,0 +1,15 @@
+/// Base Url
+const String BASE_URL_HTTP = "api.themoviedb.org";
+const String BASE_URL_HTTP_DIO = "https://api.themoviedb.org";
+
+/// End Points
+const String ENDPOINT_GET_NOW_PLAYING = "/3/movie/now_playing";
+
+/// Parameters
+const String PARAM_API_KEY = "api_key";
+const String PARAM_LANGUAGE = "language";
+const String PARAM_PAGE = "page";
+
+/// Constant Values
+const String API_KEY = "5f4bc24f61a8ea188542a598986909a6";
+const String LANGUAGE_EN_US = "en-US";
---
+++ lib/network/movie_data_agent.dart
@@ -0,0 +1,3 @@
+abstract class MovieDateAgent {
+  void getNowPlayingMovies(int page);
+}
